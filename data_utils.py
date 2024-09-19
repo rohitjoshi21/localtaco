0a38694bc3b4dce86a7cb58a216fee84f127f794
rohitjoshi21 <joshi.rk11166@gmail.com>
2024-09-19 19:24:31 +0545

    bug fixing

--- data_utils.py
+++ data_utils.py
@@ -39,7 +39,7 @@ class TextMelLoader(torch.utils.data.Dataset):
     def get_mel(self, filename):
         if not self.load_mel_from_disk:
             audio, sampling_rate = load_wav_to_torch(filename)
-            print(sampling_rate, self.stft.sampling_rate)
+            print(sampling_rate, self.stft.sampling_rate, filename)
             if sampling_rate != self.stft.sampling_rate:
                 
                 raise ValueError("{} {} SR doesn't match target {} SR".format(
